4602eed00a6c62d71a2b88c19391c0da1f5dd6e3
Willie <mulwawillie@gmail.com>
2016-08-06 05:45:14 +0300

    moreexercises

---
+++ 100.py
@@ -0,0 +1,9 @@
+print "What is you name?",
+name = raw_input()
+print "What is your color?",
+color = raw_input()
+print "What is your favorite food?",
+food = raw_input()
+
+print "So, you're %s, %s in color and %s favourite food." % (
+    name, color, food)
